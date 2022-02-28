7f0f4822e10b48cd73d2c77e06f6b302dfdce207
jjijju <cheytac406@gmail.com>
2022-02-28 17:12:16 +0900

    Set: Express Setup
    - 기본 setup

---
+++ server/api/controllers/examples/router.ts
@@ -0,0 +1,3 @@
+import express from 'express';
+import controller from './controller';
+export default express.Router().post('/', controller.create).get('/', controller.all).get('/:id', controller.byId);
